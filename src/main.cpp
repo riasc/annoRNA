1d5d56d0b7b22e223416fb843a798101d8d7128b
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-04-06 20:11:13 +0200

    included boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-#include <boost/lambda/lambda.hpp>
+#include "GeneralFeatureFormat.h"
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+using namespace std; // easy  access on STL
 int main()
 {
-    using namespace boost::lambda;
-//    typedef std::istream_iterator<int> in;
-
-    std::vector<int> nums{1,2,3,4,5,6,7,8,9,0};
-    auto print = [](const int& n) {std::cout << " " << n;};
-    //std::for_each(nums.begin(),nums.end(),print);
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "display help message")
+    ("annotation folder", po::value<vector<int>>(), "input folder")
+  ;
 
 }
-
