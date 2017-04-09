135df346733b327ede21aea90eb308375fe947cd
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-04-09 18:52:46 +0200

    small refactoring structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
+#include <boost/program_options.hpp>
 #include "GeneralFeatureFormat.h"
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 
-#include <boost/program_options.hpp>
+using namespace std; // namespace for standard library
 namespace po = boost::program_options;
-using namespace std; // easy  access on STL
-int main()
+
+int main(int argc, char *argv[])
 {
+  // declare supported options
+  int opt;
   po::options_description desc("Allowed options");
   desc.add_options()
-    ("help", "display help message")
-    ("annotation folder", po::value<vector<int>>(), "input folder")
+    ("help", "display this help message")
+    ("input-file", po::value<vector<string>>(), "input-file")  
   ;
 
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm); 
+
+  if (vm.count("help"))
+  {
+    cout << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count("input-file"))
+  {
+    cout << vm["input-file"].as<vector<string> >() 
+         << "\n"; 
+  }
+
+ 
+
+  
+
+
 }
+
