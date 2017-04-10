2a1d876f8f5f433470da9f649f69a1e3ea655e5f
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-04-10 22:15:35 +0200

    add boost fs

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,102 @@
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include "GeneralFeatureFormat.h"
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <fstream>
 
 using namespace std; // namespace for standard library
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
-int main(int argc, char *argv[])
-{
-  // declare supported options
-  int opt;
-  po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help", "display this help message")
-    ("input-file", po::value<vector<string>>(), "input-file")  
-  ;
-
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm); 
-
-  if (vm.count("help"))
-  {
-    cout << desc << "\n";
-    return 1;
-  }
-
-  if (vm.count("input-file"))
-  {
-    cout << vm["input-file"].as<vector<string> >() 
-         << "\n"; 
-  }
-
- 
-
-  
+// helper function
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
+  return os;
+}
 
 
+int main(int argc, char *argv[]) {
+     try {
+               // declare supported options
+              string config_file;
+              int opt;
+              
+              po::options_description generic("Generic Options");
+              generic.add_options()
+                ("version,v", "display version number") 
+                ("help,h", "display this help message")
+                ("config,c", po::value<string>(&config_file)->default_value("config.cfg"), "name of a configuration file") 
+              ;
+
+              // options that are allowed both on command line and in config file 
+              po::options_description config("Configuration");
+              config.add_options()
+                ("input-folder,i",
+                    po::value<string>(&config_file)->composing(), 
+                    "input-folder")
+              ;
+
+              // Hidden Options
+              po::options_description hidden("Hidden Options");
+              hidden.add_options()
+                ("optimization,o", 
+                    po::value<int>(&opt)->default_value(40), 
+                    "optimization parameter")
+              ;
+              
+              po::options_description cmdline_options;
+              cmdline_options.add(generic).add(config).add(hidden);
+
+              po::options_description config_file_options;
+              config_file_options.add(config).add(hidden);
+
+              po::options_description visible("Allowed Options");
+              visible.add(generic).add(config);
+
+              po::variables_map vm;
+              store(po::command_line_parser(argc, argv).options(cmdline_options).run(), vm);
+              notify(vm); 
+
+              ifstream ifs(config_file.c_str());
+              if(!ifs) {
+                cout << "can not open config file: " << config_file << endl;
+                return 0;
+              }
+              else {
+                store(parse_config_file(ifs, config_file_options), vm);
+                notify(vm);
+              }
+
+              if (vm.count("help")) {
+                cout << visible << "\n";
+                return 0;
+              }
+
+              if (vm.count("version")) {
+                cout << "annoRNA, version 0.1\n";
+                return 0;
+              }
+
+              if (vm.count("input-folder")) {
+                cout  << "Directory of input files "  
+                      << vm["input-folder"].as<string>()  << endl;
+              }
+/*
+              fs::path p = "/Users/";
+              if(exists(p)) {
+                cout << "exists" << endl; 
+              }
+              else {
+                cout << p << " does not exist \n"; 
+              }; */
+              
+      }
+     catch(exception& e){
+          cout << e.what() << endl;
+          return 1;
+     }
 }
 
