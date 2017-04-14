8fe31d24df20e9dc7d77ae0dbf50fd94e4e1fc0e
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-04-14 23:21:31 +0200

    added function

--- src/GeneralFeatureFormat.h
+++ src/GeneralFeatureFormat.h
@@ -1,5 +1,22 @@
+#include <string>
+#include <vector>
 class GeneralFeatureFormat 
 {
+  public:
+    GeneralFeatureFormat();
+    GeneralFeatureFormat(std::string inputfile);
+    ~GeneralFeatureFormat();
 
-}
+    void read_gff(std::string inputfile);
 
+  private:
+    std::vector<std::string> seqid;
+    std::vector<std::string> source;
+    std::vector<std::string> type;
+    std::vector<unsigned int> start;
+    std::vector<unsigned int> end; 
+    std::vector<float> score;
+    std::vector<std::string> strand;
+    std::vector<std::string> attributes;
+    
+};
