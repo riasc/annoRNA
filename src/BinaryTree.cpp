5fed77924bfb4357cf82e48579d56a513e9be83e
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-06-13 22:44:25 +0200

    treeview

--- src/BinaryTree.cpp
+++ src/BinaryTree.cpp
@@ -1,5 +1,9 @@
 #include "BinaryTree.h"
+#include "Node.h"
 #include <cstddef>
+#include <iostream>
+
+using namespace std;
 
 BinaryTree::BinaryTree() {
   root = NULL;
@@ -7,10 +11,64 @@ BinaryTree::BinaryTree() {
 
 BinaryTree::~BinaryTree() {}
 
-void BinaryTree::insert(int d) {
+void BinaryTree::insert(Node *newnode) {
+  std::cout << "insert node with key: " << newnode->key << std::endl;  
+  Node *temp = NULL;
+  Node *top = root; // cache root for traversal
+  while(top != NULL) {
+    temp = top; // copy position (current root node) 
+    if(newnode->key < top->key) {
+      top = top->left; 
+    }
+    else {
+      top = top->right; 
+    }
+  }
 
+  if(temp == NULL) {
+    root = newnode;
+  }
+  else {
+    if(newnode->key < temp->key) {
+      temp->left = newnode; 
+    }
+    else {
+      temp->right = newnode;
+    }
+  }
+}
 
+Node* BinaryTree::search(int k) {
 }
 
 
+Node* BinaryTree::getRoot() {
+  return root;
+}
 
+// prints value of root before values of subtrees
+void BinaryTree::preorder_tree_walk(Node *ptr) {
+  if(ptr != NULL) {
+    std::cout << " " << ptr->key << " " << std::endl;
+    preorder_tree_walk(ptr->left); 
+    preorder_tree_walk(ptr->right); 
+  }
+}
+
+// prints values of root between values of subtrees  
+void BinaryTree::inorder_tree_walk(Node * startnode) {
+  if(startnode != NULL) {
+    inorder_tree_walk(startnode->left); 
+    std::cout << " " << startnode->key << " ";
+    inorder_tree_walk(startnode->right); 
+  }
+}
+
+// prints values of root after values of subtrees
+void BinaryTree::postorder_tree_walk(Node * startnode) {
+  if(startnode != NULL) {
+    inorder_tree_walk(startnode->left); 
+    inorder_tree_walk(startnode->right); 
+    std::cout << " " << startnode->key << " ";
+  }
+}
